f57c4920a41b84de2624702bab99820aadfb13e0
Andrey Kurbanov <kurban1111@mail.ru>
2023-11-24 15:27:35 +0300

    first step

---
+++ WGT/connect.h
@@ -0,0 +1,16 @@
+//connect.h
+#ifndef CONNECT_H
+#define CONNECT_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+
+class connect
+{
+//    Q_OBJECT
+public:
+    connect();
+};
+
+#endif // CONNECT_H
