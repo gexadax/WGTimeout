bca2d7b5c5c88fcaa577be0639217ba826fec8b9
Andrey Kurbanov <kurban1111@mail.ru>
2024-02-17 22:07:27 +0300

    Added application icon

--- wgt_ru.ts
+++ wgt_ru.ts
@@ -5,31 +5,31 @@
     <name>DialogSettings</name>
     <message>
         <location filename="settings.ui" line="14"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_settings.h" line="127"/>
+        <location filename="build/Release/wgt_autogen/include/ui_settings.h" line="131"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="22"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_settings.h" line="128"/>
+        <location filename="settings.ui" line="26"/>
+        <location filename="build/Release/wgt_autogen/include/ui_settings.h" line="132"/>
         <source>Hostname:</source>
         <translation>Сервер:</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="48"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_settings.h" line="129"/>
+        <location filename="settings.ui" line="52"/>
+        <location filename="build/Release/wgt_autogen/include/ui_settings.h" line="133"/>
         <source>Username:</source>
         <translation>Пользователь:</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="74"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_settings.h" line="130"/>
+        <location filename="settings.ui" line="78"/>
+        <location filename="build/Release/wgt_autogen/include/ui_settings.h" line="134"/>
         <source>Password:</source>
         <translation>Пароль:</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="94"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_settings.h" line="131"/>
+        <location filename="settings.ui" line="98"/>
+        <location filename="build/Release/wgt_autogen/include/ui_settings.h" line="135"/>
         <source>Port:</source>
         <translation>Порт:</translation>
     </message>
@@ -38,55 +38,55 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="159"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="163"/>
         <source>WireGuard Timeout</source>
         <translation>WireGuard Таймаут</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="39"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="162"/>
+        <location filename="mainwindow.ui" line="43"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="166"/>
         <source>Username:</source>
         <translation>Пользователь:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="62"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="163"/>
+        <location filename="mainwindow.ui" line="66"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="167"/>
         <source>Days limit:</source>
         <translation>Лимит дней:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="89"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="164"/>
+        <location filename="mainwindow.ui" line="93"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="168"/>
         <source>Add</source>
         <translation>Добавить</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="100"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="165"/>
+        <location filename="mainwindow.ui" line="104"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="169"/>
         <source>Delete</source>
         <translation>Удалить</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="111"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="166"/>
+        <location filename="mainwindow.ui" line="115"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="170"/>
         <source>Activate</source>
         <translation>Продлить</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="131"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="167"/>
+        <location filename="mainwindow.ui" line="135"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="171"/>
         <source>Menu</source>
         <translation>Меню</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="141"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="160"/>
+        <location filename="mainwindow.ui" line="145"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="164"/>
         <source>Exit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="146"/>
-        <location filename="build/Debug/wgt_autogen/include/ui_mainwindow.h" line="161"/>
+        <location filename="mainwindow.ui" line="150"/>
+        <location filename="build/Release/wgt_autogen/include/ui_mainwindow.h" line="165"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
